23e79de9de0c8dbba4691f045866fc511ae44670
lxtrbk <alexander.triebke@gmail.com>
2023-05-07 18:40:30 +0200

    added read from file test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@ use lab_rs::LabFile;
 
 #[test]
 fn integration() {
-    let mut lbl_file = LabFile::new("test_setting");
+    let mut lbl_file: LabFile = LabFile::new("test_setting");
     lbl_file.add_label("a_lbl_name", "a_lbl_raster");
     lbl_file.add_ramcell("b_ram_name", "b_ram_raster");
     lbl_file.add_ramcell("c_ram_name", "c_ram_raster");
@@ -11,5 +11,6 @@ fn integration() {
 
 #[test]
 fn integ_from_file() {
-
+    let lbl_file: LabFile = LabFile::read_from_file("entries.lab").unwrap();
+    println!("{}", lbl_file);
 }
