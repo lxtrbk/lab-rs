920829451f57dbb108cda2ee5d8ca1aee3c459c0
lxtrbk <alexander.triebke@gmail.com>
2023-05-07 22:27:52 +0200

    integration test overhaul - added testcase for label entry without raster - added test for reading and writing to file

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,29 @@
 use lab_rs::LabFile;
+use std::fs;
 
+// test create lab file representation
 #[test]
-fn integration() {
-    let mut lbl_file: LabFile = LabFile::new("test_setting");
+fn lab_creation() {
+    let mut lbl_file: LabFile = LabFile::new("test_setting\n");
     lbl_file.add_label("a_lbl_name", "a_lbl_raster");
     lbl_file.add_ramcell("b_ram_name", "b_ram_raster");
     lbl_file.add_ramcell("c_ram_name", "c_ram_raster");
+    lbl_file.add_ramcell("d_ram_name", "");
     println!("{}", lbl_file);
 }
 
+// test read from file
+
+// test display
+
+// test write to file
 #[test]
-fn integ_from_file() {
-    let lbl_file: LabFile = LabFile::read_from_file("entries.lab").unwrap();
-    println!("{}", lbl_file);
+fn disk_read_write() {
+    let mut lbl_file1 = LabFile::read_from_file("debug_test_in.lab").unwrap();
+    lbl_file1.write("debug_test_out.lab").unwrap();
+    //println!("{}", lbl_file1);
+    let lbl_file2 = LabFile::read_from_file("debug_test_out.lab").unwrap();
+    assert_eq!(lbl_file1, lbl_file2);
+    fs::remove_file("debug_test_out.lab").unwrap();
+    //println!("end");
 }
