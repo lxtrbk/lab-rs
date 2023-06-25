ad05d348c5097386df0074b09b9ed6772a9115d2
Alexander Triebke <alexander.triebke@gmail.com>
2023-06-25 13:17:00 +0200

    - added usefull changes from branch py_wrapping

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 use std::fs::File;
-use std::io::{Write, BufReader, BufRead};
+use std::io::{BufRead, BufReader, Write};
 use std::ops::{Add, Sub};
 
 enum Section {
-	SETTINGS,
-	LABEL,
-	RAMCELL,
+    Settings,
+    Label,
+    Ramcell,
 }
 
 #[derive(PartialEq, Debug)]
@@ -19,7 +19,7 @@ struct Entry {
 
 impl Entry {
     fn new(name: String, raster: String) -> Entry {
-        Entry{name, raster}
+        Entry { name, raster }
     }
 }
 
@@ -31,154 +31,159 @@ impl fmt::Display for Entry {
 
 #[derive(PartialEq, Debug)]
 pub struct LabFile {
-	settings: String,
+    settings: String,
     label: HashMap<String, Entry>,
-	ramcell: HashMap<String, Entry>,
+    ramcell: HashMap<String, Entry>,
 }
 
 impl LabFile {
-	pub fn new(header: &str) -> LabFile {
-		let new_header: String = String::from(header.trim());
-		let new_label: HashMap<String, Entry> = HashMap::new();
-		let new_ramcell: HashMap<String, Entry> = HashMap::new();
-        LabFile{settings: new_header, label: new_label, ramcell: new_ramcell}
+    pub fn new(header: &str) -> LabFile {
+        let new_header: String = String::from(header.trim());
+        let new_label: HashMap<String, Entry> = HashMap::new();
+        let new_ramcell: HashMap<String, Entry> = HashMap::new();
+        LabFile {
+            settings: new_header,
+            label: new_label,
+            ramcell: new_ramcell,
+        }
     }
 
     pub fn add_label(&mut self, label_name: &str, label_raster: &str) {
-		let new_entry: Entry = Entry::new(String::from(label_name), String::from(label_raster));
+        let new_entry: Entry = Entry::new(String::from(label_name), String::from(label_raster));
         self.label.insert(new_entry.name.clone(), new_entry);
     }
 
-	pub fn add_ramcell(&mut self, label_name: &str, label_raster: &str) {
-		let new_entry: Entry = Entry::new(String::from(label_name), String::from(label_raster));
+    pub fn add_ramcell(&mut self, label_name: &str, label_raster: &str) {
+        let new_entry: Entry = Entry::new(String::from(label_name), String::from(label_raster));
         self.ramcell.insert(new_entry.name.clone(), new_entry);
     }
 
-	pub fn del(&mut self, label_name: &str) {
-		if self.label.contains_key(label_name) {
-			self.label.remove(label_name);
-		}
-		if self.ramcell.contains_key(label_name) {
-			self.ramcell.remove(label_name);
-		}
-	}
+    pub fn del(&mut self, label_name: &str) {
+        if self.label.contains_key(label_name) {
+            self.label.remove(label_name);
+        }
+        if self.ramcell.contains_key(label_name) {
+            self.ramcell.remove(label_name);
+        }
+    }
 
-	pub fn write(&mut self, filename: &str) -> Result<(), Box<dyn Error>> {
-		let mut output: File = File::create(filename)?;
-		write!(output, "{}", self.to_string())?;
-		Ok(())
-	}
+    pub fn write(&mut self, filename: &str) -> Result<(), Box<dyn Error>> {
+        let mut output: File = match File::create(filename) {
+            Ok(input_file) => input_file,
+            Err(error) => panic!("Problem creating new file: {:?}", error),
+        };
+        write!(output, "{self}")?;
+        Ok(())
+    }
 
-	pub fn read_from_file(filename: &str) -> Result<LabFile, Box<dyn Error>> {
-		let file: File = File::open(filename)?;
-		let reader: BufReader<File> = BufReader::new(file);
-		let mut lab_file: LabFile = LabFile::new("");
-		let mut sec: Section = Section::SETTINGS;
-		
-		for line in reader.lines() {
-			let line: String = line?;
-			if line.is_empty() {
-				// pass
-			} else if line.starts_with("[SETTINGS]") {
-				sec = Section::SETTINGS;
-			} else if line.starts_with("[LABEL]") {
-				sec = Section::LABEL;
-			} else if line.starts_with("[RAMCELL]") {
-				sec = Section::RAMCELL;
-			} else {
-				let fields: Vec<&str> = line.split(';').collect();
-				match sec {
-					Section::SETTINGS => {
-						lab_file.settings.push_str(&String::from(line.trim()));
-						lab_file.settings.push_str("\n");
-					},
-					Section::LABEL => {
-						if fields.len() > 1 {
-							lab_file.add_label(fields[0].trim(), fields[1].trim());
-						} else {
-							lab_file.add_label(fields[0].trim(), "");
-						}
-					},
-					Section::RAMCELL => {
-						if fields.len() > 1 {
-							lab_file.add_ramcell(fields[0].trim(), fields[1].trim());
-						} else {
-							lab_file.add_ramcell(fields[0].trim(), "");
-						}
-					}
-				} 
-			}
-		}
-		Ok(lab_file)
-	}
+    pub fn read_from_file(filename: &str) -> Result<LabFile, Box<dyn Error>> {
+        let file: File = File::open(filename)?;
+        let reader: BufReader<File> = BufReader::new(file);
+        let mut lab_file: LabFile = LabFile::new("");
+        let mut sec: Section = Section::Settings;
+
+        for line in reader.lines() {
+            let line: String = line?;
+            if line.is_empty() {
+                // pass
+            } else if line.starts_with("[SETTINGS]") {
+                sec = Section::Settings;
+            } else if line.starts_with("[LABEL]") {
+                sec = Section::Label;
+            } else if line.starts_with("[RAMCELL]") {
+                sec = Section::Ramcell;
+            } else {
+                let fields: Vec<&str> = line.split(';').collect();
+                match sec {
+                    Section::Settings => {
+                        lab_file.settings.push_str(&String::from(line.trim()));
+                        lab_file.settings.push('\n');
+                    }
+                    Section::Label => {
+                        if fields.len() > 1 {
+                            lab_file.add_label(fields[0].trim(), fields[1].trim());
+                        } else {
+                            lab_file.add_label(fields[0].trim(), "");
+                        }
+                    }
+                    Section::Ramcell => {
+                        if fields.len() > 1 {
+                            lab_file.add_ramcell(fields[0].trim(), fields[1].trim());
+                        } else {
+                            lab_file.add_ramcell(fields[0].trim(), "");
+                        }
+                    }
+                }
+            }
+        }
+        Ok(lab_file)
+    }
 }
 
 impl Add for LabFile {
-	type Output = Self;
+    type Output = Self;
 
     fn add(mut self, other: Self) -> Self::Output {
-		if !other.label.is_empty() {
-			for val in other.label.values() {
-				self.add_label(&val.name, &val.raster);
-			}
-		}
-		if !other.ramcell.is_empty() {
-			for val in other.ramcell.values() {
-				self.add_ramcell(&val.name, &val.raster);
-			}
-		}
-		return self;
+        if !other.label.is_empty() {
+            for val in other.label.values() {
+                self.add_label(&val.name, &val.raster);
+            }
+        }
+        if !other.ramcell.is_empty() {
+            for val in other.ramcell.values() {
+                self.add_ramcell(&val.name, &val.raster);
+            }
+        }
+        self
     }
 }
 
 impl Sub for LabFile {
-	type Output = Self;
+    type Output = Self;
 
-	fn sub(mut self, other: Self) -> Self::Output {
-		if !other.label.is_empty() {
-			for key in other.label.keys() {
-				self.del(&key);
-			}
-		}
-		if !other.ramcell.is_empty() {
-			for key in other.ramcell.keys() {
-				self.del(&key);
-			}
-		}
-		return self;
-	}
+    fn sub(mut self, other: Self) -> Self::Output {
+        if !other.label.is_empty() {
+            for key in other.label.keys() {
+                self.del(key);
+            }
+        }
+        if !other.ramcell.is_empty() {
+            for key in other.ramcell.keys() {
+                self.del(key);
+            }
+        }
+        self
+    }
 }
 
 impl fmt::Display for LabFile {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut outp = format!("[SETTINGS]\n{}\n", self.settings);
-		if !self.label.is_empty() {
-			outp.push_str("[LABEL]\n");
+        if !self.label.is_empty() {
+            outp.push_str("[LABEL]\n");
             for val in self.label.values() {
-                let entry: String;
-				if val.raster.is_empty() {
-					entry = format!("{};\n", val.name);
-				} else {
-					entry = format!("{}; {};\n", val.name, val.raster);
-				}
+                let entry = if val.raster.is_empty() {
+                    format!("{};\n", val.name)
+                } else {
+                    format!("{}; {};\n", val.name, val.raster)
+                };
                 outp.push_str(&entry);
             }
-			outp.push_str("\n");
+            outp.push('\n');
         }
-		if !self.ramcell.is_empty() {
-			outp.push_str("[RAMCELL]\n");
+        if !self.ramcell.is_empty() {
+            outp.push_str("[RAMCELL]\n");
             for val in self.ramcell.values() {
-				let entry: String;
-				if val.raster.is_empty() {
-					entry = format!("{};\n", val.name);
-				} else {
-					entry = format!("{}; {};\n", val.name, val.raster);
-				}
+                let entry: String = if val.raster.is_empty() {
+                    format!("{};\n", val.name)
+                } else {
+                    format!("{}; {};\n", val.name, val.raster)
+                };
                 outp.push_str(&entry);
             }
-			//outp.push_str("\n");
+            //outp.push_str("\n");
         }
-		write!(f, "{}", outp.trim_end_matches(", "))
+        write!(f, "{}", outp.trim_end_matches(", "))
     }
 }
 
@@ -186,16 +191,22 @@ impl fmt::Display for LabFile {
 mod tests {
     use super::*;
 
-	#[test]
-	fn new_entry () {
-		let entry1: Entry = Entry::new(String::from("test_name"), String::from("test_raster"));
-		let entry2: Entry = Entry {name: String::from("test_name"), raster: String::from("test_raster")};
-		assert_eq!(entry1, entry2);
-	}
+    #[test]
+    fn new_entry() {
+        let entry1: Entry = Entry::new(String::from("test_name"), String::from("test_raster"));
+        let entry2: Entry = Entry {
+            name: String::from("test_name"),
+            raster: String::from("test_raster"),
+        };
+        assert_eq!(entry1, entry2);
+    }
 
-	#[test]
-	fn print_entry () {
-		let entry2: Entry = Entry {name: String::from("test_name"), raster: String::from("test_raster")};
-		assert_eq!(entry2.to_string(), "(test_name, test_raster)");
-	}
+    #[test]
+    fn print_entry() {
+        let entry2: Entry = Entry {
+            name: String::from("test_name"),
+            raster: String::from("test_raster"),
+        };
+        assert_eq!(entry2.to_string(), "(test_name, test_raster)");
+    }
 }
